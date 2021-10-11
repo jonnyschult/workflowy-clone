96085adaa3108bc9dd81c48a4955baa593c6920c
Jonny <jonathon.schult@gmail.com>
2021-10-11 18:38:27 -0500

    updated app to allow the use of express middleware.

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,28 @@
-const { ApolloServer } = require("apollo-server");
+const { ApolloServer } = require("apollo-server-express");
+const express = require("express");
+const { ApolloServerPluginDrainHttpServer } = require("apollo-server-core");
 const { typeDefs } = require("./typeDefs");
 const { resolvers } = require("./resolvers");
 const dotenv = require("dotenv");
+const http = require("http");
 dotenv.config();
 
-const server = new ApolloServer({ typeDefs, resolvers });
+async function startApolloServer(typeDefs, resolvers) {
+  const app = express();
+  const httpServer = http.createServer(app);
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  await new Promise((resolve) =>
+    httpServer.listen({ port: process.env.PORT }, resolve)
+  );
+  console.log(
+    `🚀 Server ready at http://localhost:${process.env.PORT}${server.graphqlPath}`
+  );
+}
 
-server.listen({ port: process.env.PORT }).then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+startApolloServer(typeDefs, resolvers);
