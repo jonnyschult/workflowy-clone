9641e3e495435da444d05040e65de1d36e900ce4
Jonny <jonathon.schult@gmail.com>
2021-10-14 00:36:55 -0500

    Cleaned up logic for now passing user object.

--- server/src/resolvers/taskResolvers/taskQueries.js
+++ server/src/resolvers/taskResolvers/taskQueries.js
@@ -1,42 +1,36 @@
 const pool = require("../../db/db");
-const { getQueryArgs, createNestedTasks } = require("../../../utils");
 
-const getTask = async (_, args) => {
+const getTasks = async (_, __, context) => {
   try {
-    const [queryString, valArray] = getQueryArgs("select", "task", args);
-    const results = await pool.query(queryString, valArray);
-    return results.rows[0];
-  } catch (error) {
-    return {
-      success: false,
-      message: "query failed",
-      task: [],
-    };
-  }
-};
-
-const getTasks = async (_, args) => {
-  try {
-    const userId = args.userId;
+    const { user, pool } = context;
+    if (user === null) {
+      throw "You must be logged in to perform this action";
+    }
     const results = await pool.query(
       "SELECT * FROM task INNER JOIN user_task ON task.id = user_task.task_id WHERE user_task.user_id = $1;",
-      [userId]
+      [user.id]
     );
-    return results.rows;
+    if (results.rowCount === 0) {
+      throw "You have no tasks. Please create some!";
+    }
+    return {
+      success: true,
+      message: "Success",
+      tasks: results.rows,
+    };
   } catch (error) {
     console.log(error);
     return {
       success: false,
-      message: "query failed",
-      task: [],
+      message: error,
+      tasks: [],
     };
   }
 };
 
 const taskQueries = {
   Query: {
-    tasks: getTasks,
-    task: getTask,
+    getTasks,
   },
 };
 
