8c9066e16a4f4e9c4ef0a8580ce590fb8559b446
Jonny <jonathon.schult@gmail.com>
2021-10-14 00:35:38 -0500

    Added update, delete, and share resolvers.

--- server/src/resolvers/taskResolvers/taskMutations.js
+++ server/src/resolvers/taskResolvers/taskMutations.js
@@ -1,26 +1,170 @@
-const pool = require("../../db/db");
 const getQueryArgs = require("../../../utils/getQueryArgs");
 const { v4: uuid } = require("uuid");
 
-const createTask = async (_, args) => {
+const createTask = async (_, args, context) => {
   try {
-    const task = args.createTaskInput;
-    task.created_at = new Date().getTime().toString();
-    task.updated_at = new Date().getTime().toString();
+    const { user, pool } = context;
+    if (user === null) {
+      throw "You must be logged in to perform this action";
+    }
+    const { task, root_task_id } = args;
     task.id = uuid();
-    console.log(task);
+    task.owner_id = user.id;
     const [queryString, valArray] = getQueryArgs("insert", "task", task);
     const results = await pool.query(queryString, valArray);
+    await pool.query(
+      "INSERT INTO user_task (user_id, task_id) VALUES($1, $2);",
+      [user.id, task.id]
+    );
+
+    if (root_task_id === undefined) {
+      //! TODO add auto share when user creates task which has been shared with them.
+      console.log("What's good, homie?");
+    }
+
     return {
       success: true,
       message: "Task created!",
-      task: results.rows[0],
+      tasks: results.rows,
     };
   } catch (error) {
     console.log(error);
     return {
       success: false,
-      message: "query failed",
+      message: error,
+      task: null,
+    };
+  }
+};
+
+const updateTask = async (_, args, context) => {
+  try {
+    const { user, pool } = context;
+    const updateInfo = args.updateTaskInput;
+    if (user === null) {
+      throw "You must be logged in to perform this action";
+    }
+    const [selectQueryString, selectValArray] = getQueryArgs(
+      "select",
+      "user_task",
+      {
+        task_id: updateInfo.id,
+        user_id: user.id,
+      }
+    );
+    const results = await pool.query(selectQueryString, selectValArray);
+    if (results.rowsCount === 0) {
+      throw "You must be associated with this task to edit it.";
+    }
+    const [updateQueryString, updateValArray] = getQueryArgs(
+      "update",
+      "task",
+      updateInfo
+    );
+    const updateResults = await pool.query(updateQueryString, updateValArray);
+
+    return {
+      success: true,
+      message: "Task Updated.",
+      tasks: updateResults.rows,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      success: false,
+      message: error,
+      task: null,
+    };
+  }
+};
+
+const shareTask = async (_, args, context) => {
+  try {
+    const { user, pool } = context;
+    const { email, task_id } = args;
+    if (user === null) {
+      throw "You must be logged in to perform this action";
+    }
+
+    const tasksResults = await pool.query(
+      `WITH RECURSIVE children AS ( SELECT * FROM task WHERE id = $1 UNION SELECT t.* FROM task t INNER JOIN children c ON c.id = t.parent_id ) SELECT * FROM children;`,
+      [task_id]
+    );
+    console.log(tasksResults.rows[0]);
+    if (tasksResults.rows[0].owner_id !== user.id) {
+      throw "You must be the owner of this task to share it.";
+    }
+
+    const [selectUserString, selectUserArray] = getQueryArgs(
+      "select",
+      "user_account",
+      {
+        email,
+      }
+    );
+    const userResults = await pool.query(selectUserString, selectUserArray);
+
+    //regular for loop for async reasons
+    for (let i = 0; i < tasksResults.rows.length; i++) {
+      const [addQueryString, addValArray] = getQueryArgs(
+        "insert",
+        "user_task",
+        {
+          user_id: userResults.rows[0].id,
+          task_id: tasksResults.rows[i].id,
+        }
+      );
+      await pool.query(addQueryString, addValArray);
+    }
+
+    return {
+      success: true,
+      message: `User with ${email} has been add to the task.`,
+      tasks: null,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      success: false,
+      message: error,
+      task: null,
+    };
+  }
+};
+
+const deleteTask = async (_, args, context) => {
+  try {
+    const { user, pool } = context;
+    if (user === null) {
+      throw "You must be logged in to perform this action";
+    }
+    const [selectQueryString, selectValArray] = getQueryArgs(
+      "select",
+      "task",
+      args
+    );
+    const results = await pool.query(selectQueryString, selectValArray);
+    if (results.rows[0].owner_id !== user.id) {
+      throw "You must be the owner of this task to delete";
+    }
+    await getQueryArgs("delete", "task", args);
+    const [deleteQueryString, deleteValArray] = getQueryArgs(
+      "delete",
+      "task",
+      args
+    );
+    await pool.query(deleteQueryString, deleteValArray);
+
+    return {
+      success: true,
+      message: "Task deleted.",
+      tasks: null,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      success: false,
+      message: error,
       task: null,
     };
   }
@@ -28,7 +172,10 @@ const createTask = async (_, args) => {
 
 const taskMutations = {
   Mutation: {
-    createTask: createTask,
+    createTask,
+    deleteTask,
+    updateTask,
+    shareTask,
   },
 };
 
