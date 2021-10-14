54baa8ca113e960e70ae95d6d227c28611202247
Jonny <jonathon.schult@gmail.com>
2021-10-14 00:39:13 -0500

    Fleshed out logic and added proper return value.

--- server/src/resolvers/userResolvers/userQueries.js
+++ server/src/resolvers/userResolvers/userQueries.js
@@ -1,20 +1,43 @@
 const pool = require("../../db/db");
 const { getQueryArgs } = require("../../../utils");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const login = async (_, args) => {
   try {
-    const [queryString, valArray] = getQueryArgs(
-      "select",
-      "user_account",
-      args
-    );
+    const { password, email } = args;
+    const [queryString, valArray] = getQueryArgs("select", "user_account", {
+      email,
+    });
     const results = await pool.query(queryString, valArray);
-    return results.rows[0];
+    if (results.rowCount === 0) {
+      throw "No user found with that email.";
+    }
+    const user = results.rows[0];
+
+    const validPass = await bcrypt.compare(password, user.passwordhash);
+
+    if (!validPass) {
+      throw "Invalid password.";
+    }
+
+    const token = jwt.sign({ id: results.rows[0].id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    return {
+      success: true,
+      message: "Success. You are now logged in.",
+      user,
+      token,
+    };
   } catch (error) {
+    console.log(error);
     return {
       success: false,
-      message: "query failed",
-      task: [],
+      message: error,
+      user: null,
+      token: null,
     };
   }
 };
